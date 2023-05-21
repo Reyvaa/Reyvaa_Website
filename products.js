ddf92950f100a19ad04b24d6ddbb3b4cf7096c63
Dharm Patel <108889488+DharmPatelPro@users.noreply.github.com>
2023-05-21 15:54:36 +0530

    first

---
+++ products.js
@@ -0,0 +1,39 @@
+const productCardTemplate = document.querySelector("[data-product-template]")
+const productCardContainer = document.querySelector("[data-product-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+
+
+let products = []
+
+
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+
+    products.forEach(product => {
+        const isVisible = product.name.toLowerCase().includes(value) || product.type.toLowerCase().includes(value)
+        product.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+fetch("products.json")
+    .then(res => res.json())
+    .then(data => {
+        products = data.map(product => {
+            const card = productCardTemplate.content.cloneNode(true).children[0]
+
+            const type = card.querySelector("[data-type]")
+            const name = card.querySelector("[data-name]")
+            const description = card.querySelector("[data-description]")
+            const image = card.querySelector("[data-image]")
+
+            type.textContent = product.type
+            name.textContent = product.name
+            description.textContent = product.description
+            image.src = product.image
+
+            productCardContainer.append(card)
+            return { name: product.name, type: product.type, element: card }
+        })
+    })
